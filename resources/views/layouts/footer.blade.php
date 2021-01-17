0beec7b9976e20e25aabbacae90c09e09c8dc11c
shivam sharma <sshivam212@gmail.com>
2021-01-17 17:14:37 +0530

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+ <!-- Footer -->
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Rubicoit 2020</span>
+        </div>
+    </div>
+</footer>
+ <!-- End Of Footer -->    
+</body>
+
+  <!-- Bootstrap core JavaScript-->
+   
+
+
+</html>
